205cf99abfe96bc5a5604f759fdcceacca89c134
rahul4287452 <rahulgarud122@gmail.com>
2025-09-22 17:43:41 +0530

    add JWT authentication endpoints using SimpleJWT

--- bugtracker_api/bugs/serializers.py
+++ bugtracker_api/bugs/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Bug
+from rest_framework import generics
 from django.contrib.auth.models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -7,18 +8,23 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('id','username')
 
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
 class BugSerializer(serializers.ModelSerializer):
     screenshot = serializers.ImageField(use_url=True)
+    reported_by = serializers.StringRelatedField(read_only=True)
     
     class Meta:
         model = Bug
         fields = '__all__'
         read_only_fields = ['reported_by', 'created_at'] 
         
-    # def create(self, validated_data):
-    #     validated_data['reported_by'] = self.context['request'].user
-    #     return super().create(validated_data)
+    def create(self, validated_data):
+        validated_data['reported_by'] = self.context['request'].user
+        return super().create(validated_data)
 
-    # def update(self, instance, validated_data):
-    #     validated_data['reported_by'] = self.context['request'].user
-    #     return super().update(instance, validated_data)
+    def update(self, instance, validated_data):
+        validated_data['reported_by'] = self.context['request'].user
+        return super().update(instance, validated_data)
--- bugtracker_api/bugs/views.py
+++ bugtracker_api/bugs/views.py
@@ -2,11 +2,13 @@ from rest_framework import viewsets, permissions, generics, serializers
 from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from django.contrib.auth.models import User
 from .models import Bug
-from rest_framework.decorators import api_view,  parser_classes
+from rest_framework.decorators import api_view,  parser_classes, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import BugSerializer
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 
 @api_view(['GET', 'POST'])
 @parser_classes([MultiPartParser, FormParser])
@@ -19,12 +21,31 @@ def bug_list(request):
 class BugViewSet(viewsets.ModelViewSet):
     queryset = Bug.objects.all().order_by('-created_at')
     serializer_class = BugSerializer
-    permission_classes = [permissions.AllowAny]
     parser_classes = (MultiPartParser, FormParser, JSONParser)
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     def perform_create(self, serializer):
-        guest_user = User.objects.get_or_create(username="guest")[0]
-        serializer.save(reported_by=guest_user)
+        serializer.save(reported_by=self.request.user)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_user(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+    email = request.data.get('email', '')
+
+    if not username or not password:
+        return Response({'error': 'Username and password required'}, status=status.HTTP_400_BAD_REQUEST)
+
+    if User.objects.filter(username=username).exists():
+        return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+
+    user = User.objects.create_user(username=username, password=password, email=email)
+    return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+
 
 # class RegisterSerializer(serializers.ModelSerializer):
 #     class Meta:
